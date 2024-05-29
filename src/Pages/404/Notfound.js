c75649d3a1711ce411d0b46ed772237acbd1a5fa
shanmuganarayanan <shanmuga05111999@gmail.com>
2024-05-29 21:53:57 +0530

    navigation path changed

--- src/Pages/404/Notfound.js
+++ src/Pages/404/Notfound.js
@@ -1,25 +1,19 @@
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { ApiLoaderHandler } from "../../actions/apiLoaderActions";
+import { useNavigate } from "react-router-dom";
 
 
 const Notfound = () => {
 
     const dispatch = useDispatch();
 
+    const navigate = useNavigate();
+
     useEffect(() => {
         dispatch(ApiLoaderHandler(false));
+        navigate('/');
     },[])
-
-    return(
-        <div className="h-full w-full flex items-center text-center justify-center">
-            <div className="max-w-lg w-full">
-                <p className="text-9xl text-rose-700">404</p>
-                <p className="mt-3 text-2xl text-gray-100 font-medium">An error occurred. Please try again later.</p>
-                <p className="mt-6 text-xl text-gray-500">We are unable to process your request right now. Please try again after sometime.</p>
-            </div>
-        </div>
-    )
 }
 
 export default Notfound;
