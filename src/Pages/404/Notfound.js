3f6b8b6530127a626a99aed3566db4193094a5bc
shanmuganarayanan <shanmuga05111999@gmail.com>
2024-05-29 21:25:10 +0530

    New details updated

--- src/Pages/404/Notfound.js
+++ src/Pages/404/Notfound.js
@@ -1,14 +1,18 @@
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { ApiLoaderHandler } from "../../actions/apiLoaderActions";
+import { useNavigate } from "react-router-dom";
 
 
 const Notfound = () => {
 
     const dispatch = useDispatch();
 
+    const navigate = useNavigate();
+
     useEffect(() => {
         dispatch(ApiLoaderHandler(false));
+        navigate('/');
     },[])
 
 
