82e8b3b0e227a0bc04025aa5a73e862e2f1782ce
shanmuganarayanan <shanmuga05111999@gmail.com>
2024-06-01 21:23:57 +0530

    button navigation added

--- src/Pages/Features/Features.js
+++ src/Pages/Features/Features.js
@@ -56,7 +56,7 @@ const Features = () => {
                                 <path fill-rule="evenodd" d="M4.5 5.653c0-1.427 1.529-2.33 2.779-1.643l11.54 6.347c1.295.712 1.295 2.573 0 3.286L7.28 19.99c-1.25.687-2.779-.217-2.779-1.643V5.653Z" clip-rule="evenodd" />
                             </svg>
                     </button>
-                    <button className="absolute top-24 gap-1 items-center text-bold text-white w-16 h-16 text-white flex justify-center items-center font-medium text-lg " onClick={homeHandler}>
+                    <button className="absolute top-24 gap-1 items-center text-bold text-white w-16 h-16 text-white flex justify-center items-center font-medium text-lg hidden lg:flex" onClick={homeHandler}>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
                             </svg>
